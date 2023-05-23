9aedde620c68c76568264b6099594a30563d6ac0
Joshua Gato <joshuagato37@gmail.com>
2023-05-23 13:46:23 +0000

    Added tests for core app

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -9,6 +9,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTest(SimpleTestCase):
     """Test commands."""
@@ -25,8 +26,8 @@ class CommandTest(SimpleTestCase):
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
         patched_check.side_effect = [Psycopg2Error] * 2 + \
-            [OperationalError] * 3 + [true]
-        
+            [OperationalError] * 3 + [True]
+
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
