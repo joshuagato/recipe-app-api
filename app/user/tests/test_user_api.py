305b8451324464c917b15687216857ce49c26c4e
Joshua Gato <joshuagato37@gmail.com>
2023-05-24 23:56:09 +0000

    Added tests for user creation process

---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,56 @@
+"""
+Tests for the user API.
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+
+CREATE_USER_URL = reverse('user:create')
+
+def create_user(**params):
+    """Create and return a new user."""
+    return get_user_model().objects.create_user(**params)
+
+class PublicUserApiTests(TestCase):
+    """Test the public features of the user API."""
+
+    def setUp(self):
+        self.client = APIClient()
+        self.payload = {
+            'email': 'test@example.com',
+            'password': 'testpass',
+            'name': 'Test Name'
+        }
+
+    def test_create_user_success(self):
+        """Test creating a user is successful."""
+        res = self.client.post(CREATE_USER_URL, self.payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(email=self.payload['email'])
+        self.assertTrue(user.check_password(self.payload['password']))
+        self.assertNotIn('password', res.data)
+
+    def test_user_with_email_exists_error(self):
+        """Test error returned if user with email exists."""
+        create_user(**self.payload)
+
+        res = self.client.post(CREATE_USER_URL, self.payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short_error(self):
+        """Test an error is returned if password is less than 5 characters."""
+        self.payload['password'] = 'pw'
+
+        res = self.client.post(CREATE_USER_URL, self.payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email=self.payload['email']
+        ).exists()
+        self.assertFalse(user_exists)
